4922002895a3f306df5d6286b16ac8b7f07b7513
Periklis Ntanasis <pntanasis@gmail.com>
2014-01-26 05:30:41 +0200

    moved the array manipulation and search methods in separate Class and made them public for easier testing

---
+++ src/Cryptolexo/ArrayHelpers.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Cryptolexo;
+
+/**
+ *
+ * @author periklis
+ */
+public class ArrayHelpers {
+    
+    public static int getRowWithCollision(String[][] array, String word, int r) {
+        for(int i=0;i<array[0].length-word.length();i++) {
+            // start from all possible places
+            int arrayPointer = i;
+            int wordPointer = 0;
+            while(wordPointer < word.length() && arrayPointer < array[0].length) {
+                if(array[r][arrayPointer] == null || 
+                        array[r][arrayPointer].equals(String.valueOf(word.charAt(wordPointer)))) {
+                    wordPointer++;
+                    if(wordPointer==word.length()) {
+                        return arrayPointer - word.length() +1;//because ???
+                    }
+                } else {
+                    wordPointer = 0;
+                }
+                arrayPointer++;
+            }            
+        }
+        return -1;
+    }
+    
+    public static int getColWithCollision(String[][] array, String word, int c) {
+        // what is this for for? can't remmeber and so sleepy :/
+        for(int i=0;i<array.length-word.length();i++) {
+            // start from all possible places
+            int arrayPointer = i;
+            int wordPointer = 0;
+            while(wordPointer < word.length() && arrayPointer < array.length) {
+                if(array[arrayPointer][c] == null || 
+                        array[arrayPointer][c].equals(String.valueOf(word.charAt(wordPointer)))) { // redo this with substring
+                    wordPointer++;
+                    if(wordPointer==word.length()) {
+                        return arrayPointer - word.length() +1;
+                    }
+                } else {
+                    wordPointer = 0;
+                }
+                arrayPointer++;
+            }            
+        }
+        return -1;
+    }
+    
+    public static int getColEmptySpace(String[][] array, int length, int c) {
+        int count = 0;
+        for(int i=0;i<array[0].length;i++) {
+            if(array[i][c] == null) {
+                count++;
+                if(count == length) {
+                    return (i-count+1); // WHY???
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return -1;
+    }
+    
+    public static int getRowEmptySpace(String[][] array, int length, int r) {
+        int count = 0;
+        for(int i=0;i<array[0].length;i++) {
+            if(array[r][i] == null) {
+                count++;
+                if(count == length) {
+                    return (i-count+1);
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return -1;
+    }
+    
+    public static boolean isColEmpty(String[][] array, int c) {
+        for(int i=0;i<array.length;i++) {
+            if(array[i][c] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isRowEmpty(String[][] array, int r) {
+        for(int i=0;i<array.length;i++) {
+            if(array[r][i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void addVerticalWord(String[][] array, int n,int m,String word) {
+        for(int i=0;i<word.length();i++) {
+            array[i+n][m] = ""+word.charAt(i);
+        }
+    }
+    
+    public static void addHorizontalWord(String[][] array, int n,int m,String word) {
+        for(int i=0;i<word.length();i++) {
+            array[n][i+m] = ""+word.charAt(i);
+        }
+    }
+    
+}
--- src/Cryptolexo/Cryptolexo.java
+++ src/Cryptolexo/Cryptolexo.java
@@ -6,6 +6,14 @@
 
 package Cryptolexo;
 
+import static Cryptolexo.ArrayHelpers.addHorizontalWord;
+import static Cryptolexo.ArrayHelpers.addVerticalWord;
+import static Cryptolexo.ArrayHelpers.getColEmptySpace;
+import static Cryptolexo.ArrayHelpers.getColWithCollision;
+import static Cryptolexo.ArrayHelpers.getRowEmptySpace;
+import static Cryptolexo.ArrayHelpers.getRowWithCollision;
+import static Cryptolexo.ArrayHelpers.isColEmpty;
+import static Cryptolexo.ArrayHelpers.isRowEmpty;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -71,24 +79,24 @@ public class Cryptolexo {
         if(Utils.random(2) == 0) {
             // Vertical
             int col = Utils.random(M);
-            if(isColEmpty(col)) {
+            if(isColEmpty(cryptolexo, col)) {
                 int row = Utils.random(N-word.length());
-                addVerticalWord(row,col,word);
+                addVerticalWord(cryptolexo, row,col,word);
                 return true;
             } else {
                 //TRY TO FILL THE SPACES - FIRST AVAILABLE WILL DO
-                int row = getColEmptySpace(word.length(), col);
+                int row = getColEmptySpace(cryptolexo, word.length(), col);
                 if(row >= 0) {
                     System.out.println("HERE YES!!! "+row+" "+col+" "+word);
-                    addVerticalWord(row,col,word);
+                    addVerticalWord(cryptolexo, row,col,word);
                     return true;
                 } else {
                     // TRY TO FILL IT WITH COLLISIONS!
                     System.out.println("Trying colllision col");
-                    row = getColWithCollision(word, col);
+                    row = getColWithCollision(cryptolexo, word, col);
                     if(row > 0) {
                         System.out.println("7777 YES!!! "+row+" "+col+" "+word);
-                        addVerticalWord(row,col,word);
+                        addVerticalWord(cryptolexo, row,col,word);
                         return true;
                     }
                 }
@@ -96,143 +104,31 @@ public class Cryptolexo {
         } else {
             // Horizontal
             int row = Utils.random(N);
-            if(isRowEmpty(row)) {
+            if(isRowEmpty(cryptolexo, row)) {
                 int col = Utils.random(M-word.length());
-                addHorizontalWord(row,col,word);
+                addHorizontalWord(cryptolexo, row,col,word);
                 return true;
             } else {
                 //TRY TO FILL THE SPACES - FIRST AVAILABLE WILL DO
-                int col = getRowEmptySpace(word.length(), row);
+                int col = getRowEmptySpace(cryptolexo, word.length(), row);
                 System.out.println("Trying empy row"+row);
                 if(col >= 0) {
                     System.out.println("HERE222 YES!!! "+row+" "+col+" "+word);
-                    addHorizontalWord(row,col,word);
+                    addHorizontalWord(cryptolexo, row,col,word);
                     return true;
                 } else {
                     // TRY TO FILL IT WITH COLLISIONS!
                     System.out.println("Trying colllision row");
-                    col = getRowWithCollision(word, row);
+                    col = getRowWithCollision(cryptolexo, word, row);
                     if(col > 0) {
                         System.out.println("3333 YES!!! "+row+" "+col+" "+word);
-                        addHorizontalWord(row,col,word);
+                        addHorizontalWord(cryptolexo, row,col,word);
                         return true;
                     }
                 }
             }
         }
         return false;
-    }
-    
-    private int getRowWithCollision(String word, int r) {
-        System.out.println("getRowWithCollision "+word+" "+r);
-        for(int i=0;i<cryptolexo[0].length-word.length();i++) {
-            // start from all possible places
-            int arrayPointer = i;
-            int wordPointer = 0;
-            while(wordPointer < word.length() && arrayPointer < cryptolexo[0].length) {
-                if(cryptolexo[r][arrayPointer] == null || 
-                        cryptolexo[r][arrayPointer].equals(String.valueOf(word.charAt(wordPointer)))) {
-                    wordPointer++;
-                    if(wordPointer==word.length()) {
-                        return arrayPointer - word.length() +1;//because ???
-                    }
-                } else {
-                    wordPointer = 0;
-                }
-                arrayPointer++;
-            }            
-        }
-        return -1;
-    }
-    
-    private int getColWithCollision(String word, int c) {
-        System.out.println("getColWithCollision "+word+" "+c);
-        // what is this for for? can't remmeber and so sleepy :/
-        for(int i=0;i<cryptolexo.length-word.length();i++) {
-            // start from all possible places
-            int arrayPointer = i;
-            int wordPointer = 0;
-            System.out.println("LOOP");
-            while(wordPointer < word.length() && arrayPointer < cryptolexo.length) {
-                System.out.println("Arr Point : "+arrayPointer);
-                System.out.println("c : "+c);
-                if(cryptolexo[arrayPointer][c] == null || 
-                        cryptolexo[arrayPointer][c].equals(String.valueOf(word.charAt(wordPointer)))) { // redo this with substring
-                    wordPointer++;
-                    if(wordPointer==word.length()) {
-                        return arrayPointer - word.length() +1;
-                    }
-                } else {
-                    wordPointer = 0;
-                }
-                arrayPointer++;
-                System.out.println("wp : "+wordPointer);
-            }            
-        }
-        return -1;
-    }
-    
-    private int getColEmptySpace(int length, int c) {
-        System.out.println("getColEmptySpace "+c+" "+length);
-        int count = 0;
-        for(int i=0;i<cryptolexo[0].length;i++) {
-            if(cryptolexo[i][c] == null) {
-                System.out.println(">>> i="+i+" cryptolexo[i][c]"+cryptolexo[i][c]+" count"+count);
-                count++;
-                if(count == length) {
-                    return (i-count+1); // WHY???
-                }
-            } else {
-                count = 0;
-            }
-        }
-        return -1;
-    }
-    
-    private int getRowEmptySpace(int length, int r) {
-        System.out.println("getRowEmptySpace "+r+" "+length);
-        int count = 0;
-        for(int i=0;i<cryptolexo[0].length;i++) {
-            if(cryptolexo[r][i] == null) {
-                count++;
-                if(count == length) {
-                    return (i-count+1);
-                }
-            } else {
-                count = 0;
-            }
-        }
-        return -1;
-    }
-    
-    private boolean isColEmpty(int c) {
-        for(int i=0;i<cryptolexo.length;i++) {
-            if(cryptolexo[i][c] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private boolean isRowEmpty(int r) {
-        for(int i=0;i<cryptolexo.length;i++) {
-            if(cryptolexo[r][i] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private void addVerticalWord(int n,int m,String word) {
-        for(int i=0;i<word.length();i++) {
-            cryptolexo[i+n][m] = ""+word.charAt(i);
-        }
-    }
-    
-    private void addHorizontalWord(int n,int m,String word) {
-        for(int i=0;i<word.length();i++) {
-            cryptolexo[n][i+m] = ""+word.charAt(i);
-        }
     }
     
     public void printCryptolexo() {
